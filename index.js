a1a5b6eb5feb5af21f50569917d52e8e7ec472cc
Noone <shobonaoc@gmail.com>
2021-01-27 12:15:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', async msg => {
+  if (msg.author.username === '竜介軍二等兵') {
+    
+  }
+  if (msg.content === 'メンバー') {
+    var guild = msg.guild;
+    var members = await guild.members.fetch();
+    var ryusukeMembers = members.filter(member => {
+      var name = member.nickname;
+      if (name == null) {
+        name = member.user.username
+      }
+      return name.includes("竜介軍") && member.presence.status !== 'offline'
+    })
+    msg.reply(`ただいま${ryusukeMembers.size}人です。`)
+  }
+});
+
+client.login(BOT_TOKEN);
