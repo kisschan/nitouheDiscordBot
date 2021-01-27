747cb764318195b12f4f3f116ef97cf773aaa8ea
Noone <shobonaoc@gmail.com>
2021-01-28 01:45:49 +0900

    抽選機能てきとう

--- index.js
+++ index.js
@@ -10,7 +10,47 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var onPlaying = false;
+var requestBox = {};
+var cancelVoted = null;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+const playMusic = function(msg, url) {
+  if (!url) return;
+  let stream = ytdl(url, {
+    filter: "audioonly",
+    opusEncoded: true,
+    encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+  });
+
+  msg.member.voice.channel.join()
+  .then(connection => {
+    onPlaying = true;
+    let dispatcher = connection.play(stream, {
+      type: "opus"
+    })
+    .on("finish", () => {
+      if (requestBox.size !== 0) {
+        keys = Object.keys(requestBox)
+        var index = getRandomInt(keys.length);
+        var next = requestBox[keys[index]];
+        requestBox = {};
+        msg.channel.send(next)
+        playMusic(msg, next);
+      }
+      onPlaying = false;
+      if (requestBox.size === 0) { 
+        msg.guild.me.voice.channel.leave(); 
+      }
+    })
+  });
+}
+
 client.on('message', async msg => {
+  if (msg.author.bot) return;
   if (msg.content === 'はさみ将棋') {
     msg.reply(`https://sdin.jp/browser/board/hasami/`)
   }
@@ -27,22 +67,27 @@ client.on('message', async msg => {
     msg.reply(`ただいま${ryusukeMembers.size}人です。`)
   }
   if (msg.content.includes("youtu")) {
-    if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
-        let stream = ytdl(msg.content, {
-            filter: "audioonly",
-            opusEncoded: true,
-            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-        });
-        
-        msg.member.voice.channel.join()
-        .then(connection => {
-            let dispatcher = connection.play(stream, {
-                type: "opus"
-            })
-            .on("finish", () => {
-                msg.guild.me.voice.channel.leave();
-            })
-        });
+    if (!msg.member.voice.channel) {
+      return msg.reply("配信ルームに入ってからリクエストしてください。");
+    }
+    if (onPlaying) {
+      // 抽選箱に入れる
+      requestBox[msg.member.id] = msg.content;
+      return msg.reply(msg.member.id + " 抽選箱に入れました。")
+    }
+    playMusic(msg, msg.content)
+  }
+  if (msg.content === "cancel") {
+    if (cancelVoted === null) {
+      cancelVoted = msg.member.id;
+      return msg.reply("キャンセル投票を受け付けました。もうひとりの投票でキャンセルになります。")
+    }
+    if (cancelVoted !== msg.member.id) {
+      cancelVoted = null;
+      msg.guild.me.voice.channel.leave(); 
+      onPlaying = false;
+      return msg.reply("キャンセル投票が二人以上あったため、キャンセルします。")
+    }
   }
 });
 
