e250ba390bbbf1dd2b46295bdad3e336c93e4066
Noone <shobonaoc@gmail.com>
2021-01-27 19:11:41 +0900

    ytdlの追加

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
+const ytdl = require("discord-ytdl-core");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -24,6 +25,24 @@ client.on('message', async msg => {
       return name.includes("竜介軍") && member.presence.status !== 'offline'
     })
     msg.reply(`ただいま${ryusukeMembers.size}人です。`)
+  }
+  if (msg.content.includes("youtu")) {
+    if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
+        let stream = ytdl(msg.content, {
+            filter: "audioonly",
+            opusEncoded: true,
+            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+        });
+        
+        msg.member.voice.channel.join()
+        .then(connection => {
+            let dispatcher = connection.play(stream, {
+                type: "opus"
+            })
+            .on("finish", () => {
+                msg.guild.me.voice.channel.leave();
+            })
+        });
   }
 });
 
