83c58e6b2d15111038b5ed8957040a4d39d85ce7
kisschan <fabehu692@macr2.com>
2021-02-23 19:08:45 +0900

    　体力機能を付けた

--- nitouheReplier.js
+++ nitouheReplier.js
@@ -2,6 +2,8 @@ class NitouheReplier {
 
   constructor() {
     this.status = false;
+    this.HP = 10;
+
   }
   
   on() {
@@ -15,28 +17,43 @@ class NitouheReplier {
   isAvailable() {
     return this.status;
   }
-  
-  async onMessage(msg) {
-    if(msg.content === '!on'){
-      this.on();
-    }else if(msg.content === '!off'){
-      this.off();
-    }
-    if (!this.isAvailable())
-      return;
-    if(/(?!\?)(?:はい|入)る[わよか]?$/?.test(msg.content)){
-      msg.reply('おうはいれ')
-    }else if(/(?!\?)(?:[すスｽ]る)[わよか]?$/.test(msg.content)){
-      msg.reply('おうしろ')
-    }else if(/(?:たまちゃん|tama)/.test(msg.content)){
-      msg.reply('なにがたまちゃんじゃい！')
-    }else if(/(?:乞食|[こコｺ][じジｼﾞ][きキｷ]|[死氏市４4しシｼ]ね|[う失]せろ|[消きキｷ][えエｴ][ろロﾛ]|([くクｸ][さサｻ]|臭)い)/.test(msg.content)){
-      const randomreply = ['いえいえ','むっ','そういう言葉は控えましょう','さて'];
-      msg.reply(randomreply[Math.floor(Math.random() * randomreply.length)])
-    }
-    
+  isHP(){
+    return this.HP;
   }
-
-}
+  deleteHP() {
+    this.HP--;
+  }
+   async onMessage(msg) {
+          if(msg.content === '!on'){
+            this.on();
+            msg.reply('はいおは')
+          }else if(msg.content === '!off'){
+            this.off();
+          }
+          if (!this.isAvailable())
+            return;
+          if(this.isHP()<1){
+            msg.reply('ではねます')
+            this.off();
+            return;
+          }
+          if(/(?!\?)(?:はい|入)る[わよか]?$/?.test(msg.content)){
+            msg.reply('おうはいれ')
+            this.deleteHP();
+          }else if(/(?!\?)(?:[すスｽ]る)[わよか]?$/.test(msg.content)){
+            msg.reply('おうしろ')
+            this.deleteHP();
+          }else if(/(?:たまちゃん|tama)/.test(msg.content)){
+            msg.reply('なにがたまちゃんじゃい！')
+            this.deleteHP();
+          }else if(/(?:乞食|[こコｺ][じジｼﾞ][きキｷ]|[死氏市４4しシｼ]ね|[う失]せろ|[消きキｷ][えエｴ][ろロﾛ]|([くクｸ][さサｻ]|臭)い)/.test(msg.content)){
+            const randomreply = ['いえいえ','むっ','そういう言葉は控えましょう','さて'];
+            msg.reply(randomreply[Math.floor(Math.random() * randomreply.length)])
+            this.deleteHP();
+          }
+      
+        }
+      
+      }
 
 export default NitouheReplier;
