473151d15a5856be649f54bd9cb4ca2b42ebe8d2
Noone <shobonaoc@gmail.com>
2021-02-23 20:48:58 +0900

    [add] stateパターン実装

--- Service/yamanote.js
+++ Service/yamanote.js
@@ -1,11 +1,52 @@
 export class Yamanote {
 
   constructor() {
-    console.log('created')
+    this.state = yamanoteStateTypes.stop;
+    this.mode = null;
   }
 
   async onMessage(msg) {
-    console.log('onMessage');
+    switch(this.state) {
+      case yamanoteStateTypes.stop:
+        this.onMessageStopped(msg);
+      case yamanoteStateTypes.select:
+        this.onMessageSelecting(msg);
+      case yamanoteStateTypes.ongoing:
+        this.onMessageOngoing(msg);
+    }
   }
 
+  onMessageStopped(msg) {
+    if (msg.content === 'yamanote') {
+      msg.reply('山手線ゲームはじまるどー');
+      this.state = yamanoteStateTypes.select
+    }
+  }
+
+  onMessageSelecting(msg) {
+    if (msg.content === 'country') {
+      msg.reply('国モードだお〜');
+      this.state = yamanoteStateTypes.ongoing;
+    }
+  }
+
+  onMessageOngoing(msg) {
+    if (msg.content === '日本') {
+      msg.react('🇯🇵');
+      msg.reply('うーむｗ')
+      this.state = yamanoteStateTypes.stop;
+    }
+  }
+
+}
+
+const yamanoteStateTypes = {
+  stop: 'stop',
+  select: 'select',
+  ongoing: 'ongoing',
+  result: 'result'
+}
+
+const yamanoteModeTypes = {
+  country: 'country'
 }
