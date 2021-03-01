e46d471443451a3584f78e9d663a9ca190039d3b
kisschan <fabehu692@macr2.com>
2021-03-02 01:22:38 +0900

    にとうへぼっとは現在の体力を即時に表すように。さとことエブリワンロールはブロックリストに入りやすくHPも減りやすくなるように。疲労度の二乗分初期の体力から減るようにした

--- nitouheReplier.js
+++ nitouheReplier.js
@@ -4,7 +4,7 @@ class NitouheReplier {
 
   constructor() {
     this.status = false;
-    this.HP = 10;
+    this.HP = 20;
     this.count = 1;
     this.arashiarray =[];
     this.blocklistarray=[];
@@ -33,7 +33,7 @@ class NitouheReplier {
     return this.blocklistarray.includes(msg.author.id);
   }
   deleteHP(msg) {
-    if(msg.member.displayName === 'Amateur"キスケ"' || msg.member.roles.cache.size < 2){
+    if(msg.member.id === '719528011707449436' || msg.member.roles.cache.size < 2 || msg.member.id === "813000558503788584" || msg.member.id === '812703512407834686'){
     this.HP = this.HP-2;
     }else{
     this.HP--;}
@@ -46,22 +46,26 @@ class NitouheReplier {
   }
 
   resur(msg){
-    msg.reply('ﾆﾄｳﾍは戻ってきたようだ');
+    msg.reply(`ﾆﾄｳﾍは戻ってきたようだ(疲労度:${this.count})`);
     this.status = true;
-    this.HP = 10;
     this.count++;
+    if(this.count<4){
+      this.HP = 20-(this.count*this.count);
+    }else{
+      this.HP = 5;
+    }
     setTimeout(() => {
       this.count--;
-    },1000*60*15)
+    },1000*60*30)
   }
 
   arashi(msg){
     this.arashiarray.push(msg.author.id);
     let arashicount = this.arashiarray.filter((x) => {return x===msg.author.id}).length;
-    if(arashicount > 4){
+    if(arashicount > 9){
       this.blocklist(msg);
     }
-    if(msg.member.roles.cache.size > 1){
+    if(msg.member.roles.cache.size > 1 || msg.member.id !== "813000558503788584" || msg.member.id !== '812703512407834686'){
       setTimeout(() => {
         if(this.arashiarray.length>0){
         this.arashiarray.shift();
@@ -78,9 +82,9 @@ class NitouheReplier {
    async onMessage(msg) {
 
         if(msg.guild.id === '794882838666543114'){
-         var emoji = `HP(${this.isHP()})${msg.guild.emojis.cache.find(e => e.name === 'nitouhe')}<`;
+         var emoji = `${msg.guild.emojis.cache.find(e => e.name === 'nitouhe')}<`;
          }else if(msg.guild.id === '804641873847255051'){
-         var emoji = `HP(${this.isHP()})${msg.guild.emojis.cache.find(e => e.name === 'anzen_kisuke')}<`;
+         var emoji = `${msg.guild.emojis.cache.find(e => e.name === 'anzen_kisuke')}<`;
          }
     
         if(this.isBlocklist(msg))
@@ -94,42 +98,45 @@ class NitouheReplier {
           }
           if (!this.isAvailable())
             return;
+
+          if(/(?!\?)(?:はい|入)る[わよか]?$/?.test(msg.content)){
+            this.deleteHP(msg);
+            msg.reply(`HP(${this.isHP()})${emoji}おうはいれ`)
+          }else if(/(?!\?)(?:[すスｽ]る)[わよか]?$/.test(msg.content)){
+            this.deleteHP(msg);
+            msg.reply(`HP(${this.isHP()})${emoji}おうしろ`)
+          }else if(/(?:たまちゃん|tama)/.test(msg.content)){
+            this.deleteHP(msg);
+            msg.reply(`HP(${this.isHP()})${emoji}なにがたまちゃんじゃい！`)
+          }else if(/(?:乞食|[こコｺ][じジｼﾞ][きキｷ]|[死氏市４4しシｼ]ね|[う失]せろ|[消きキｷ][えエｴ][ろロﾛ]|([くクｸ][さサｻ]|臭)い)/.test(msg.content)){
+            const randomreply = ['いえいえ','むっ','そういう言葉は控えましょう','さて'];
+            this.deleteHP(msg);
+            msg.reply(`HP(${this.isHP()})${emoji}` + randomreply[Math.floor(Math.random() * randomreply.length)])
+          }
+
           if(this.isHP() <1){
             const random = Math.floor(Math.random()*3)+1;
           if(random === 1){
-            msg.reply(`${emoji}うんこしてくる`)
+            msg.reply(`HP(${this.isHP()})${emoji}うんこしてくる`)
             this.addHP(60*this.iscount(), () => {
               this.resur(msg);
             });
+            return;
           }else if(random === 2){
-            msg.reply(`${emoji}飯食うわ`)
+            msg.reply(`HP(${this.isHP()})${emoji}飯食うわ`)
             this.addHP(60*2*this.iscount(), () => {
               this.resur(msg);
             });
+            return;
           }else if(random === 3){
-            msg.reply(`${emoji}ではねます`)
+            msg.reply(`HP(${this.isHP()})${emoji}ではねます`)
             this.addHP(60*3*this.iscount(), () => {
               this.resur(msg);
             });
             return;
           }
-        
-          }
-
-          if(/(?!\?)(?:はい|入)る[わよか]?$/?.test(msg.content)){
-            msg.reply(`${emoji}おうはいれ`)
-            this.deleteHP(msg);
-          }else if(/(?!\?)(?:[すスｽ]る)[わよか]?$/.test(msg.content)){
-            msg.reply(`${emoji}おうしろ`)
-            this.deleteHP(msg);
-          }else if(/(?:たまちゃん|tama)/.test(msg.content)){
-            msg.reply(`${emoji}なにがたまちゃんじゃい！`)
-            this.deleteHP(msg);
-          }else if(/(?:乞食|[こコｺ][じジｼﾞ][きキｷ]|[死氏市４4しシｼ]ね|[う失]せろ|[消きキｷ][えエｴ][ろロﾛ]|([くクｸ][さサｻ]|臭)い)/.test(msg.content)){
-            const randomreply = ['いえいえ','むっ','そういう言葉は控えましょう','さて'];
-            msg.reply(`${emoji}` + randomreply[Math.floor(Math.random() * randomreply.length)])
-            this.deleteHP(msg);
-          }
+      
+        }
       
         }
       
