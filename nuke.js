fe706ab7e19e838de7867285060d002dd2f93c53
iwamizawa-software <iwamizawasoftware@icloud.com>
2021-03-14 13:23:32 +0900

    一応banしようとしてみる

--- nuke.js
+++ nuke.js
@@ -28,17 +28,12 @@ class Nuke {
       let msgs = await channel.messages.fetch(options);
       if (!msgs.size)
         return;
-      msgs.each(msg => msg.member.id === id && msg.delete().catch(()=>0));
+      channel.bulkDelete(msgs.filter(msg => msg.member.id === id)).catch(()=>0);
       lastId = msgs.last().id;
     }
   }
   
   async onMessage(msg) {
-    console.log([
-      this.isAvailable,
-      msg.member.id,
-      this.banList[msg.member.id]
-    ]);
     if (this.isAvailable && this.banList[msg.member.id]) {
       msg.delete();
       return;
@@ -54,8 +49,10 @@ class Nuke {
         msg.delete();
       } else if (this.isAvailable && BANCOMMAND.test(msg.content)) {
         const unban = RegExp.$1, id = RegExp.$2;
-        if (this.banList[id] = !unban)
+        if (this.banList[id] = !unban) {
+          msg.guild.members.ban(id, {days: 7}).catch(()=>0);
           this.deleteLog(msg.channel, id);
+        }
         msg.delete();
       }
     }
