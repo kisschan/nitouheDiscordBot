515384aca00fc6efac348694eaf3e923ba948087
iwamizawa-software <iwamizawasoftware@icloud.com>
2021-03-23 20:09:07 +0900

    banしたときサーバーのチャンネルすべてからログを消す　遠隔ロールバックできないのを修正

--- nuke.js
+++ nuke.js
@@ -25,17 +25,19 @@ class Nuke {
       this.timerId = setTimeout(this.setMode.bind(this, false), PERIOD);
   }
   
-  async deleteLog(channel, id) {
+  deleteLog(guild, id) {
     let options = {limit: 100}, lastId;
-    for (var i = 0; i < MAX_DELETE; i += options.limit) {
-      if (lastId)
-        options.before = lastId;
-      let msgs = await channel.messages.fetch(options);
-      if (!msgs.size)
-        return;
-      channel.bulkDelete(msgs.filter(msg => msg.member.id === id)).catch(()=>0);
-      lastId = msgs.last().id;
-    }
+    guild.channels.cache.each(async channel => {
+      for (var i = 0; i < MAX_DELETE; i += options.limit) {
+        if (lastId)
+          options.before = lastId;
+        let msgs = await channel.messages.fetch(options);
+        if (!msgs.size)
+          return;
+        channel.bulkDelete(msgs.filter(msg => msg.member.id === id)).catch(()=>0);
+        lastId = msgs.last().id;
+      }
+    });
   }
   
   async onMessage(msg) {
@@ -62,7 +64,7 @@ class Nuke {
         if (this.banList[id] = !unban) {
           if (this.isAvailable) {
             guild.members.ban(id, {days: 7}).catch(err => log(guild.name + 'にてbanできねえ', err.stack));
-            this.deleteLog(msg.channel, id);
+            this.deleteLog(guild, id);
             log(LOG_TITLE, guild.name + 'にて' + msg.member.user.tag + 'が' + id + 'をBAN');
           } else {
             log(LOG_TITLE, guild.name + 'にて' + msg.member.user.tag + 'が' + id + 'をBANしようとしたが緊急事態宣言期間中でないため失敗');
@@ -87,7 +89,7 @@ class Nuke {
       } else if (ROLLBACK.test(msg.content)) {
         const channel = (RegExp.$1 && msg.client.channels.cache.find(channel => channel.id === RegExp.$1)) || msg.channel;
         const filtered = (await channel.messages.fetch({ limit: 100 })).filter(msg => (msg.member.roles && msg.member.roles.cache.size < 2) || msg.author.bot);
-        msg.channel.bulkDelete(filtered);
+        channel.bulkDelete(filtered);
         msg.delete();
         log(channel.name + 'にてロールバック実行', msg.member.user.tag);
       }
