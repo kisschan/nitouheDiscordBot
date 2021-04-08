c5c804f12b8d80e3b1f5484ce4f3d37b84c804f5
kisschan <fabehu692@macr2.com>
2021-04-09 04:43:49 +0900

    オリジナル役職機能追加

--- Service/moneys.js
+++ Service/moneys.js
@@ -11,6 +11,11 @@ class Bank extends BaseBot {
       this.Moneymultiple = 1;
       this.PreUNIX = 0;
       this.Autoboost = 0;
+      this.createrole_Lv = 0;
+      this.createrole_userId = '';
+      this.createrole_Name = '';
+      this.createrole_Wrongcolor = false;
+      this.createrole_Color = '';
     }
 
     ismoney(){
@@ -31,6 +36,26 @@ class Bank extends BaseBot {
 
     isautoboost(){
       return this.Autoboost;
+    }
+
+    iscreate_roleLv(){
+      return this.createrole_Lv;
+    }
+
+    iscreate_userid(){
+      return this.createrole_userId;
+    }
+
+    iscreate_rolename(){
+      return this.createrole_Name;
+    }
+
+    iscreate_wrongcolor(){
+      return this.createrole_Wrongcolor;
+    }
+
+    iscreate_rolecolor(){
+      return this.createrole_Color;
     }
 
 
@@ -60,10 +85,131 @@ class Bank extends BaseBot {
       this.Autoboost = 0;
     }
 
+    createrole_Lvadd(){
+      this.createrole_Lv++;
+    }
+
+    createrole_Lvdown(){
+      this.createrole_Lv--;
+    }
+
+    createrole_Lvset(Lv){
+      this.createrole_Lv = Lv;
+    }
+
+    createrole_userid(msg){
+      this.createrole_userId = msg.member.id;
+    }
+
+    createrole_name(msg){
+      this.createrole_Name = msg.content;
+    }
+
+    createrole_wrongcolor(){
+      this.createrole_Wrongcolor = true;
+    }
+
+    createrole_color(msg){
+      if(msg.content === 'ホワイト'){
+      this.createrole_Color = 'WHITE';
+      }else if(msg.content === 'アクア'){
+       this.createrole_Color = 'AQUA' ;
+      }else if(msg.content === 'グリーン'){
+       this.createrole_Color = 'GREEN';
+      }else if(msg.content === 'ブルー'){
+        this.createrole_Color = 'BLUE';
+      }else if(msg.content === 'イエロー'){
+        this.createrole_Color = 'YELLOW';
+      }else if(msg.content === '鮮やかなピンク'){
+        this.createrole_Color = 'LUMINOUS_VIVID_PINK'
+      }else if(msg.content === 'ゴールド'){
+        this.createrole_Color = 'GOLD';
+      }else if(msg.content === 'レッド'){
+        this.createrole_Color = 'RED';
+      }else if(msg.content === 'グレー'){
+        this.createrole_Color = 'GREY';
+      }
+    };
+
+    reset_craterole(){
+    this.createrole_Lv = 0;
+    this.createrole_userId = '';
+    this.createrole_Name = '';
+    this.createrole_Wrongcolor = false;
+    this.createrole_Color = '';
+    }
+
   
   
-    onMessage(msg) {
+    async onMessage(msg) {
     super.onMessage(msg);
+    if(/^[$＄]オリジナル$/.test(msg.content) && this.iscreate_roleLv() === 0){
+      this.addmoney(-3000);
+      const money = this.ismoney();
+    this.userRecordRepository.findMoneyByDiscordId(msg.member.id, (err, result) => {
+      if(err) return;
+      if(result.reduce((a, c) => a + c.money, 0) + money < 0){
+       msg.react('⚠');
+       return;
+      }
+      　})
+    this.createrole_userid(msg);
+    this.createrole_Lvadd();
+    msg.reply('作りたいロールの名前を言ってください');
+    
+    }else if(/^(?:[きキｷ][ゃャｬ][んンﾝ][せセｾ][るルﾙ])$/.test(msg.content) && this.iscreate_roleLv() > 0){
+     msg.reply('キャンセルされました');
+     this.reset_craterole();
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 1){
+    this.createrole_name(msg);
+    msg.reply(`作りたいロールは${this.iscreate_rolename()}でよろしいですか❓\nはいかいいえでお願いします。`)
+    this.createrole_Lvadd();
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 2 && msg.content !== 'いいえ' && msg.content !== 'はい'){
+      msg.reply('はいかいいえで答えないと進みません');
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 2 && msg.content === 'いいえ'){
+     this.createrole_Lvdown(); 
+     msg.reply('もう一回作りたいロール名をお願い致します')
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 2 && (msg.content === 'はい' || this.iscreate_wrongcolor())){
+      msg.reply(`次に作りたいロールの色を以下の中からお願い致します。\nホワイト\nアクア\nグリーン\nブルー\nイエロー\n鮮やかなピンク\nゴールド\nレッド\nグレー`)
+    this.createrole_Lvadd();
+    }else if(!/^(ホワイト|アクア|グリーン|ブルー|イエロー|鮮やかなピンク|ゴールド|レッド|グレー)$/.test(msg.content) && msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 3){
+    this.createrole_Lvdown();
+    this.createrole_wrongcolor();
+    msg.reply('不正な値です。なにか話すとロールの色選択に戻ります')
+    }else if(/^(ホワイト|アクア|グリーン|ブルー|イエロー|鮮やかなピンク|ゴールド|レッド|グレー)$/.test(msg.content) && msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 3){
+      this.createrole_color(msg);
+      this.createrole_Lvadd();
+      msg.reply(`作りたいロールの名前(${this.iscreate_rolename()})\n色(${this.iscreate_rolecolor()})でよろしいですね？\nはいかいいえでお願いします\n※色が英語になるのは仕様です`);
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 4 && msg.content !== 'いいえ' && msg.content !== 'はい'){
+      msg.reply('はいかいいえで答えないと進みません');
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 4 && msg.content === 'いいえ'){
+      this.createrole_Lvset(1);
+      msg.reply('ロールの名前からもう一度お伺いします');
+    }else if(msg.member.id === this.iscreate_userid() && this.iscreate_roleLv() === 4 && msg.content === 'はい'){
+    const Role = await msg.guild.roles.create({
+    data:{
+    name: this.iscreate_rolename(),
+    color: this.iscreate_rolecolor(),
+     },
+    reason:'要請があったため',
+    })
+    msg.member.roles.add(Role);
+    this.addmoney(-3000);
+    const userId = this.iscreate_userid();
+    const money = this.ismoney(); 
+    this.userRecordRepository.findMoneyByDiscordId(msg.member.id, (err, result) => {
+    if(err) return;
+    if(result.reduce((a, c) => a + c.money, 0) + money < 0){
+    msg.react('⚠');
+    return;
+    }
+      　})
+    this.userRecordRepository.addMoneyscore(userId, money, err => {
+    msg.react(err?'⚠':'💸');
+    msg.reply(err?'購入できせん':'購入できました');
+    });
+    this.reset_craterole();
+    }
     if(msg.guild.id === '804641873847255051' || msg.guild.id === '822064180219084820'){
     const userId = msg.member.id
     if(this.ispreUNIX() !== 0){
